17ef72036b034911a5265bf83b51ecec7fecf84e
AJ Murray <aj.murr4y@gmail.com>
2023-01-06 15:23:56 -0500

    completed leetcode 1 in python3 and JS

---
+++ leetCode/1-two-sum/pySolution.py
@@ -0,0 +1,47 @@
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+# You can return the answer in any order.
+
+# Example 1:
+# Input: nums = [2,7,11,15], target = 9
+# Output: [0,1]
+# Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+# Example 2:
+# Input: nums = [3,2,4], target = 6
+# Output: [1,2]
+
+# Example 3:
+# Input: nums = [3,3], target = 6
+# Output: [0,1]
+
+# Constraints:
+# 2 <= nums.length <= 104
+# -109 <= nums[i] <= 109
+# -109 <= target <= 109
+# Only one valid answer exists.
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        prevMap = {}
+
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in prevMap:
+                return [prevMap[diff], i]
+            prevMap[n] = i 
+        return
+
+# different solution using pointers 
+# failed the time complexity constraints of O(n^2) but passed 55/57 tests
+        # self.indexes = []
+        # if len(nums) == 2:
+        #     return ([0,1])
+        # else:
+        #     for i in range(0,len(nums)):
+        #         for j in range(len(nums) -1 , 0, -1):
+        #             if nums[i] + nums[j] == target and not i == j:
+        #                 print(i, j)
+        #                 self.indexes.append(i)
+        #                 self.indexes.append(j)
+        #                 return self.indexes
